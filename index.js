8fff01d4693956926f358aeab0f2968d129a59f7
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 19:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser ;
 const welcomeMessage = "Welcome to Flatbook, "+currentUser+"!";
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, "+currentUser.charAt(0)+"!";
