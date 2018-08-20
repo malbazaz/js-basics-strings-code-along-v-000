07a3cc1d7b6058f37dbf0ded05ebebfbda9f1fea
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 19:11:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Makak" ;
+const welcomeMessage = "Welcome to Flatbook, "+currentUser+"!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, "+currentUser.charAt(0)+"!";
